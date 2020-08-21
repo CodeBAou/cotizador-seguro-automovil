630cb91b4e067658b7cfe05889af46dd7921f0e3
CodeBAou <borisafou@gmail.com>
2020-08-21 12:39:00 +0200

    content_form_responsive

--- src/App.js
+++ src/App.js
@@ -11,7 +11,15 @@ function App() {
         <header>
           <h1 className="Titulo_Page">Seguro</h1> <h1 className="Titulo_Page cero-rotate">0</h1>
         </header>
-       
+        
+        <div className="content_form">
+
+              <h2>Calcula tu Seguro</h2>
+
+             
+
+        </div>
+
       </div>
 
     </div>
